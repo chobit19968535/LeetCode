c4c6bfb103bed52f509824d1f4a9bd5212723aaa
Lyciris <41323791+chobit19968535@users.noreply.github.com>
2025-04-22 10:55:13 +0800

    加入專案檔案。

---
+++ Easy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Easy
+    {
+        public int[] ID_0001_TwoSum(int[] nums, int target)
+        {
+            int[] ans;
+            Dictionary<int, int> pair = new Dictionary<int, int>();
+            Dictionary<int, int> log = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++) 
+            { 
+                int value = target - nums[i];
+                int key = nums[i];
+
+                if (pair.ContainsKey(value))
+                {
+                    ans = new int[2] { log[value], i };
+                    return ans;
+                }
+                else
+                {
+                    if (pair.ContainsKey(key)) continue;
+                    pair.Add(key, value);
+                    log.Add(key, i);
+                }
+
+            }
+            ans = new int[2] { -1, -1};
+            return ans;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Easy easy = new Easy();
+            int[] nums = new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
+            int target = 11;
+
+            var ans = easy.ID_0001_TwoSum(nums, target);
+            Console.WriteLine(ans[0]);
+            Console.WriteLine(ans[1]);
+
+            Console.WriteLine();
+        }
+    }
+}
